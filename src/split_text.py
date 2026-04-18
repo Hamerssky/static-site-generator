d7e6984e14bf22534e27b30a859a8d3ae3a54527
Hamerssky <hamerssky@gmail.com>
2026-04-18 23:52:22 +0300

    Node split by link and image

--- src/split_text.py
+++ src/split_text.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extract_text import extract_markdown_links, extract_markdown_images
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     # TODO: Add support for multiple delimeters in one text 
@@ -17,5 +18,53 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(new_node, TextType.TEXT))
             else:
                 new_nodes.append(TextNode(new_node, text_type))
+
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes: list[TextNode] = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        remaining = node.text
+        images = extract_markdown_images(remaining)
+
+        for alt, url in images:
+            before, remaining = remaining.split(f"![{alt}]({url})", 1)
+
+            if before:
+                new_nodes.append(TextNode(before, TextType.TEXT))
+
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+
+        if remaining:
+            new_nodes.append(TextNode(remaining, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes: list[TextNode] = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        remaining = node.text
+        links = extract_markdown_links(remaining)
+
+        for text, url in links:
+            before, remaining = remaining.split(f"[{text}]({url})", 1)
+
+            if before:
+                new_nodes.append(TextNode(before, TextType.TEXT))
+
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+
+        if remaining:
+            new_nodes.append(TextNode(remaining, TextType.TEXT))
 
     return new_nodes
--- src/test_split_text.py
+++ src/test_split_text.py
@@ -1,5 +1,5 @@
 import unittest
-from split_text import split_nodes_delimiter
+from split_text import split_nodes_delimiter, split_nodes_link, split_nodes_image
 from textnode import TextNode, TextType
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -55,3 +55,146 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
 
         self.assertEqual(result, [])
+
+
+class TestSplitNodesLinksImages(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_images_back_to_back(self):
+        node = TextNode(
+            "![a](url1)![b](url2)",
+            TextType.TEXT,
+        )
+        result = split_nodes_image([node])
+
+        self.assertEqual(result, [
+            TextNode("a", TextType.IMAGE, "url1"),
+            TextNode("b", TextType.IMAGE, "url2"),
+        ])
+
+    def test_image_at_start(self):
+        node = TextNode(
+            "![img](url) text after",
+            TextType.TEXT,
+        )
+        result = split_nodes_image([node])
+
+        self.assertEqual(result, [
+            TextNode("img", TextType.IMAGE, "url"),
+            TextNode(" text after", TextType.TEXT),
+        ])
+
+    def test_image_at_end(self):
+        node = TextNode(
+            "text before ![img](url)",
+            TextType.TEXT,
+        )
+        result = split_nodes_image([node])
+
+        self.assertEqual(result, [
+            TextNode("text before ", TextType.TEXT),
+            TextNode("img", TextType.IMAGE, "url"),
+        ])
+
+    def test_no_images(self):
+        node = TextNode("just text", TextType.TEXT)
+        result = split_nodes_image([node])
+
+        self.assertEqual(result, [node])
+
+    def test_image_non_text_passthrough(self):
+        node = TextNode("![img](url)", TextType.BOLD)
+        result = split_nodes_image([node])
+
+        self.assertEqual(result, [node])
+
+    def test_multiple_links(self):
+        node = TextNode(
+            "a [one](url1) b [two](url2)",
+            TextType.TEXT,
+        )
+        result = split_nodes_link([node])
+
+        self.assertEqual(result, [
+            TextNode("a ", TextType.TEXT),
+            TextNode("one", TextType.LINK, "url1"),
+            TextNode(" b ", TextType.TEXT),
+            TextNode("two", TextType.LINK, "url2"),
+        ])
+
+    def test_link_at_start(self):
+        node = TextNode(
+            "[link](url) rest",
+            TextType.TEXT,
+        )
+        result = split_nodes_link([node])
+
+        self.assertEqual(result, [
+            TextNode("link", TextType.LINK, "url"),
+            TextNode(" rest", TextType.TEXT),
+        ])
+
+    def test_link_at_end(self):
+        node = TextNode(
+            "before [link](url)",
+            TextType.TEXT,
+        )
+        result = split_nodes_link([node])
+
+        self.assertEqual(result, [
+            TextNode("before ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "url"),
+        ])
+
+    def test_no_links(self):
+        node = TextNode("plain text", TextType.TEXT)
+        result = split_nodes_link([node])
+
+        self.assertEqual(result, [node])
+
+    def test_image_not_link(self):
+        node = TextNode("![img](url)", TextType.TEXT)
+        result = split_nodes_link([node])
+
+        self.assertEqual(result, [node])
+
+    # Pipeline dependency, won't work unless processed in strict order images -> links
+    def test_image_link_mix(self):
+        node = TextNode("![text](url)[text](url)", TextType.TEXT)
+
+        nodes = split_nodes_image([node])
+        nodes = split_nodes_link(nodes)
+
+        self.assertEqual(nodes, [
+            TextNode("text", TextType.IMAGE, "url"),
+            TextNode("text", TextType.LINK, "url"),
+        ])
+
+    def test_mixed_image_and_text(self):
+        node = TextNode(
+            "text ![img](url) more text",
+            TextType.TEXT,
+        )
+        result = split_nodes_image([node])
+
+        self.assertEqual(result, [
+            TextNode("text ", TextType.TEXT),
+            TextNode("img", TextType.IMAGE, "url"),
+            TextNode(" more text", TextType.TEXT),
+        ])
