c5d3d9415877c2ad53260652b46c5e85edb8b60e
Hamerssky <hamerssky@gmail.com>
2026-04-18 20:44:29 +0300

    Text node to html + unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,7 @@
+from textnode import TextType
+
 class HTMLNode:
-    """HTML node class, represents inline or bloc level snippets like html tags and text"""
+    """HTML node class, represents inline or block level snippets like html tags and text"""
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value
@@ -21,7 +23,7 @@ class HTMLNode:
         return f"Tag: {self.tag}\nValue: {self.value}\nChildren: {self.children}\nProps: {self.props}\n"
     
 class LeafNode(HTMLNode):
-    """Leaf node class, represents last nodes in an html tree structure"""
+    """Leaf node class, represents last nodes in an HTML tree structure"""
     def __init__(self, tag, value, props = None):
         super().__init__(tag, value, None, props)
 
@@ -33,9 +35,7 @@ class LeafNode(HTMLNode):
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
-        return f"Tag: {self.tag}\n\
-            Value: {self.value}\n\
-            Props: {self.props}\n"
+        return f"Tag: {self.tag}\nValue: {self.value}\nProps: {self.props}\n"
         
 class ParentNode(HTMLNode):
     """Parent node class, represents nodes with children"""
@@ -55,3 +55,27 @@ class ParentNode(HTMLNode):
         res += f"</{self.tag}>"
 
         return res
+    
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise Exception("Node is not of valid type")
+
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+
+    raise Exception("Unknown TextType")
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from htmlnode import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,6 +18,46 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node2, node4)
         self.assertNotEqual(node4, node5)
         self.assertEqual(node5, node6)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "bold text")
+
+
+    def test_italic(self):
+        node = TextNode("italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "italic text")
+
+
+    def test_code(self):
+        node = TextNode("code()", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "code()")
+
+
+    def test_link(self):
+        node = TextNode("click me", TextType.LINK, "https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "click me")
+        self.assertEqual(html_node.props, {"href": "https://example.com"})
+
+    def test_image(self):
+        node = TextNode("Some alt text", TextType.IMAGE, "https://img.com/pic.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.props, {"src": "https://img.com/pic.png", "alt": "Some alt text"})
 
 
 if __name__ == "__main__":
